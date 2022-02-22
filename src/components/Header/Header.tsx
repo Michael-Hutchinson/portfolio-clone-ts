a955bc8dad54dedddd378bed93bf393c76b8ff7b
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 21:35:00 +0000

    add state for handling scroll and add background color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Navbar from '../shared/Navbar/Navbar';
 import { Top } from './Header.styles';
 
 const Header: React.FunctionComponent = () => {
+  const navClass = [];
+  const [scroll, setScroll] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 50) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  if (scroll) {
+    navClass.push('navBackground');
+  }
+
   return (
-    <Top>
+    <Top className={navClass.join()}>
       <Navbar />
     </Top>
   );
