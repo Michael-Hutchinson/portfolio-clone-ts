f346ae0a92ba0a98222aed6d43affdec749440a0
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 21:49:55 +0000

    add social styling and social icons

---
+++ src/components/shared/Social/Social.styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  bottom: 0;
+  left: 1.25rem;
+  position: fixed;
+  z-index: 10;
+  @media (max-width: 48rem) {
+    position: relative;
+    left: 0;
+  }
+`;
+
+export const Icon = styled.ul`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  &:after {
+    background-color: var(--light-slate);
+    content: '';
+    height: 6.25rem;
+    width: 0.063rem;
+  }
+  li {
+    &:last-of-type {
+      margin-bottom: 1.25rem;
+    }
+    a {
+      color: var(--gold);
+      display: flex;
+      padding: 0.625rem;
+      transition: all ease-in-out 0.4s;
+      &:hover {
+        transform: scale(1.5);
+      }
+    }
+  }
+  @media (max-width: 48rem) {
+    flex-direction: row;
+    justify-content: center;
+    li {
+      &:last-of-type {
+        margin-bottom: 0;
+      }  
+    }
+    &:after {
+      height: initial;
+      width: initial;
+    }
+`;
--- src/components/shared/Social/Social.tsx
+++ src/components/shared/Social/Social.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+
+import { Icon, Wrapper } from './Social.styles';
 
 const Social: React.FunctionComponent = () => {
-  return <p>test</p>;
+  return (
+    <Wrapper>
+      <Icon>
+        <li>
+          <a href="twitter" target="_blank" rel="noopender noreferrer">
+            <FaTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="github" target="_blank" rel="noopender noreferrer">
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a href="linkedin" target="_blank" rel="noopender noreferrer">
+            <FaLinkedinIn />
+          </a>
+        </li>
+      </Icon>
+    </Wrapper>
+  );
 };
 
 export default Social;
