52392059e105d7318b3b36ebba1e6761f33e9757
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 20:26:32 +0000

    create navbar

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
 
-const Navbar = () => {
+import cv from '../../../images/cv.jpeg';
+import Button from '../Button/Button';
+import { Input, Label, Links, MobileLinks, Nav } from './Navbar.styles';
+
+const Navbar: React.FunctionComponent = () => {
   return (
-    <div>
-      <p>navbar</p>
-    </div>
+    <Nav>
+      <Input id="nav-responsive" />
+      <div>
+        <p>Hello</p>
+      </div>
+      <div>
+        <Label htmlFor="nav-responsive">
+          <FaBars />
+        </Label>
+      </div>
+      <MobileLinks>
+        <Links href="#home">Home</Links>
+        <Links href="#about">About me</Links>
+        <Links href="#work">My work</Links>
+        <Links href="#contact">Contact</Links>
+        <Button links={cv} buttonText="Download My CV" />
+      </MobileLinks>
+    </Nav>
   );
 };
 
