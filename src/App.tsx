738298d7b089bab35f3c6e94ea7786835c9d52e9
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 20:33:58 +0000

    add header to app instead of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import GlobalStyle from 'App.styles';
 import React from 'react';
 
 import About from './components/About/About';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Navbar from './components/shared/Navbar/Navbar';
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <GlobalStyle />
-      <Home />
-      <About />
-      <Navbar />
+      <Header />
+      <main>
+        <Home />
+        <About />
+      </main>
     </div>
   );
 };
