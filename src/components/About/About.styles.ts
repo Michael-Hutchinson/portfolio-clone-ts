c0228865869f88bcbaa369639813a51707cd1261
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 16:53:44 +0000

    create about page and styling

---
+++ src/components/About/About.styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-gap: 3.125rem;
+  @media (max-width: 48rem) {
+    display: block;
+  }
+`;
+
+export const ListItems = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(8.75rem, 12.5rem));
+  list-style: none;
+  margin: 1.25rem 0 0 0;
+  overflow: hidden;
+  padding: 0;
+  li {
+    position: relative;
+    margin-bottom: 0.625rem;
+    padding-left: 1.25rem;
+    font-family: var(--font-mono);
+    font-size: 0.813rem;
+    &:before {
+      color: #e3ae4a;
+      content: '▹';
+      left: 0;
+      position: absolute;
+    }
+  }
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+  border: 0.063rem solid #e3ae4a;
+  border-radius: 0.25rem;
+  padding: 0.313rem;
+`;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
+import logo from '../../images/me.png';
+import { ListItems, Logo, Wrap } from './About.styles';
+
 const About: React.FunctionComponent = () => {
-  return <div>about page</div>;
+  return (
+    <section id="about">
+      <h2>A little more about me</h2>
+      <Wrap>
+        <div>
+          <p>
+            Experienced Web Developer with a history of working in the IT industry.
+            Skilled in Web Design & Development including HTML, CSS & Javascript.
+          </p>
+          <p>
+            Strong UX portfolio with a Master’s degree in Information Technology from
+            Leeds University.
+          </p>
+          <p>
+            If you want to know more just email me at michael-hutchinson@hotmail.co.uk or
+            use the contact form at the bottom of the page.
+          </p>
+          <p>TL;DR? Here are a few technologies I’ve been working with recently:</p>
+          <ListItems>
+            <li>Javascript (ES6+)</li>
+            <li>Node.js</li>
+            <li>Cypress</li>
+            <li>React</li>
+            <li>HTML & (S)CSS</li>
+            <li>XML & JSON</li>
+          </ListItems>
+        </div>
+        <div>
+          <Logo src={logo} alt="me" />
+        </div>
+      </Wrap>
+    </section>
+  );
 };
 
 export default About;
