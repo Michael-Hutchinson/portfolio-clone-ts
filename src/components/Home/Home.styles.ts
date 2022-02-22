0a97040b28d983598af5ad4118145bf068f0e57d
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 16:25:37 +0000

    create homepage component

---
+++ src/components/Home/Home.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Name = styled.h5`
+  color: #e3ae4a;
+  font-family: 'Roboto Mono', monospace;
+  margin: 0 0 0 0.25rem;
+  font-size: 0.875rem;
+`;
+
+export const Title = styled.h1`
+  font-size: clamp(2.5rem, 6vw, 5rem);
+  font-weight: bold;
+  margin: 0;
+`;
+
+export const SubTitle = styled.h2`
+  color: #8892b0;
+  font-size: clamp(2.5rem, 6vw, 4rem);
+  margin: 0;
+`;
+
+export const Text = styled.p`
+  margin: 1.25rem 0 3.125rem;
+`;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import Button from 'components/shared/Button/Button';
+import React from 'react';
+
+import { Name, SubTitle, Text, Title } from './Home.styles';
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <section>
+      <Name>Hi, my name is</Name>
+      <Title>Michael Hutchinson.</Title>
+      <SubTitle>I like to code things for the web.</SubTitle>
+      <Text>A design-minded front-end web developer based in Manchester.</Text>
+      <Button links="#about" buttonText="Learn more" />
+    </section>
+  );
+};
+
+export default Home;
