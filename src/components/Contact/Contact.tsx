f0f602bb1ecc809431eaea5ae44cf5a64d9ed055
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-23 10:57:42 +0000

    WIP: add contact wording

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Title from '../shared/Title/Title';
+
 const Contact: React.FunctionComponent = () => {
-  return <div>contact form here</div>;
+  return (
+    <section id="contact">
+      <Title titleText="Get in touch" />
+      <h3>
+        If you have any questions or want to know more, please use the contact form below
+      </h3>
+    </section>
+  );
 };
 
 export default Contact;
