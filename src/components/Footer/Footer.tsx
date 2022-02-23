cd1377c708aaec3e0676dcc4cba59009b1574cd9
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-23 08:42:01 +0000

    create footer component and pass in social icons

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Social from '../shared/Social/Social';
+
+const Footer: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Social />
+    </div>
+  );
+};
+
+export default Footer;
