4bea3412ddcc7180dde4a182621cb1524d8b19d1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-24 15:10:55 +0000

    update modal type

--- src/components/shared/Modal/Modal.tsx
+++ src/components/shared/Modal/Modal.tsx
@@ -5,13 +5,14 @@ import { ModalButton, ModalImage, ModalOpen, Wrapper } from './Modal.styles';
 interface IModalProps {
   alt: string;
   image: string;
+  toggleModal: (e: React.MouseEvent<HTMLInputElement>) => void;
 }
 
-const Modal: React.FunctionComponent<IModalProps> = ({ alt, image }) => {
+const Modal: React.FunctionComponent<IModalProps> = ({ alt, image, toggleModal }) => {
   return (
     <Wrapper>
       <ModalOpen>
-        <ModalButton defaultValue="Close" />
+        <ModalButton onClick={toggleModal} defaultValue="Close" />
         <ModalImage src={image} alt={alt} />
       </ModalOpen>
     </Wrapper>
