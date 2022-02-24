d58c32f35f18b09c48bfdc50b2e31b932992d9d0
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-24 13:26:35 +0000

    add toggle modal function

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import abbey from '../../images/work/abbey-work.png';
 import aetc from '../../images/work/aetc-work.png';
@@ -11,32 +11,102 @@ import morality from '../../images/work/morality-work.png';
 import peopletracing from '../../images/work/people-tracing-work.png';
 import portfolio from '../../images/work/portfolio-work.png';
 import runner from '../../images/work/runner-work.png';
+import Modal from '../shared/Modal/Modal';
 import Title from '../shared/Title/Title';
 import { ImageWrap } from './Work.styles';
 
 const Work: React.FunctionComponent = () => {
+  const [modal, setModal] = useState<boolean>(false);
+  const [image, setImage] = useState<string>('');
+  const toggleModal = (e: React.MouseEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setImage(target.alt);
+    setModal(!modal);
+  };
   return (
     <section id="work">
       <Title titleText="Projects i&#39;ve built" />
       <h3>I design and build websites for clients. More work avaliable on request.</h3>
       <ImageWrap>
-        <input type="image" className="modalImage" alt={abbey} src={abbey} />
-        <input type="image" className="modalImage" alt={aetc} src={aetc} />
-        <input type="image" className="modalImage" alt={betting} src={betting} />
-        <input type="image" className="modalImage" alt={hsscp} src={hsscp} />
-        <input type="image" className="modalImage" alt={leeds} src={leeds} />
-        <input type="image" className="modalImage" alt={lscb} src={lscb} />
-        <input type="image" className="modalImage" alt={midwife} src={midwife} />
-        <input type="image" className="modalImage" alt={morality} src={morality} />
+        <input
+          type="image"
+          className="modalImage"
+          alt={abbey}
+          src={abbey}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={aetc}
+          src={aetc}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={betting}
+          src={betting}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={hsscp}
+          src={hsscp}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={leeds}
+          src={leeds}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={lscb}
+          src={lscb}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={midwife}
+          src={midwife}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={morality}
+          src={morality}
+          onClick={toggleModal}
+        />
         <input
           type="image"
           className="modalImage"
           alt={peopletracing}
           src={peopletracing}
+          onClick={toggleModal}
         />
-        <input type="image" className="modalImage" alt={portfolio} src={portfolio} />
-        <input type="image" className="modalImage" alt={runner} src={runner} />
+        <input
+          type="image"
+          className="modalImage"
+          alt={portfolio}
+          src={portfolio}
+          onClick={toggleModal}
+        />
+        <input
+          type="image"
+          className="modalImage"
+          alt={runner}
+          src={runner}
+          onClick={toggleModal}
+        />
       </ImageWrap>
+      {modal ? <Modal alt={image} image={image} toggleModal={toggleModal} /> : null}
     </section>
   );
 };
