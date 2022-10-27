61c7a76226dc8f523996acad5707b4f8aafa6dd6
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-27 22:41:46 +0100

    feat: add dark mode toggle

---
+++ src/components/shared/Darkmode/Darkmode.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+const Darkmode = (): (string | (() => void))[] => {
+  const [theme, setTheme] = useState('light');
+
+  const setMode = (mode: string) => {
+    window.localStorage.setItem('theme', mode);
+    setTheme(mode);
+  };
+
+  const themeToggler = () => {
+    if (theme === 'light') {
+      setMode('dark');
+    } else {
+      setMode('light');
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches &&
+      !localTheme
+    ) {
+      setMode('dark');
+    } else if (localTheme) {
+      setTheme(localTheme);
+    } else {
+      setMode('light');
+    }
+  }, []);
+  return [theme, themeToggler];
+};
+
+export default Darkmode;
