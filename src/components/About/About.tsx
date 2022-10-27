8a4dde973dcc618e48a39f9642598592760e7cba
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-27 17:17:25 +0100

    feat: add config in about section
    - map through config wording

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,35 +1,25 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
+import config from '../../data/config.json';
 import logo from '../../images/me.png';
 import Title from '../shared/Title/Title';
 import { ListItems, Logo, Wrap } from './About.styles';
 
-const About: React.FunctionComponent = () => {
+const About = (): ReactElement => {
+  const aboutWording = config.sections.about.p;
+  const techList = config.sections.about.list;
   return (
     <section id="about">
-      <Title titleText="A little more about me" />
+      <Title titleText={config.sections.about.title} />
       <Wrap>
         <div>
-          <p>
-            Experienced Web Developer with a history of working in the IT industry.
-            Skilled in Web Design & Development including HTML, CSS & Javascript.
-          </p>
-          <p>
-            Strong UX portfolio with a Master’s degree in Information Technology from
-            Leeds University.
-          </p>
-          <p>
-            If you want to know more just email me at michael-hutchinson@hotmail.co.uk or
-            use the contact form at the bottom of the page.
-          </p>
-          <p>TL;DR? Here are a few technologies I’ve been working with recently:</p>
+          {aboutWording.map((p) => (
+            <p key={p}>{p}</p>
+          ))}
           <ListItems>
-            <li>Javascript (ES6+)</li>
-            <li>Node.js</li>
-            <li>Cypress</li>
-            <li>React</li>
-            <li>HTML & (S)CSS</li>
-            <li>XML & JSON</li>
+            {techList.map((list) => (
+              <li key={list}>{list}</li>
+            ))}
           </ListItems>
         </div>
         <div>
