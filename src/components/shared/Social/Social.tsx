179bf34dc02b63d937fcc54e054db72f30e9d249
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-10-27 22:45:31 +0100

    feat: add data links to socials

--- src/components/shared/Social/Social.tsx
+++ src/components/shared/Social/Social.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 
+import config from '../../../data/config.json';
 import { Icon, Wrapper } from './Social.styles';
 
-const Social: React.FunctionComponent = () => {
+const Social = (): ReactElement => {
   return (
     <Wrapper>
       <Icon>
         <li>
-          <a href="twitter" target="_blank" rel="noopender noreferrer">
+          <a
+            href={config.socialMedia.twitter.url}
+            target="_blank"
+            rel="noopender noreferrer">
             <FaTwitter />
           </a>
         </li>
         <li>
-          <a href="github" target="_blank" rel="noopender noreferrer">
+          <a
+            href={config.socialMedia.github.url}
+            target="_blank"
+            rel="noopender noreferrer">
             <FaGithub />
           </a>
         </li>
         <li>
-          <a href="linkedin" target="_blank" rel="noopender noreferrer">
+          <a
+            href={config.socialMedia.linkedin.url}
+            target="_blank"
+            rel="noopender noreferrer">
             <FaLinkedinIn />
           </a>
         </li>
