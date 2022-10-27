ae88c6eb004124d769886c2e6db91badf4bed584
Michael Hutchinson <Michael.Hutchinson@thrivelearning.com>
2022-10-27 17:14:24 +0100

    feat: use config wording on home section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
+import config from '../../data/config.json';
 import Button from '../shared/Button/Button';
 import { Name, SubTitle, Text, Title } from './Home.styles';
 
-const Home: React.FunctionComponent = () => {
+const Home = (): ReactElement => {
   return (
     <section id="home">
-      <Name>Hi, my name is</Name>
-      <Title>Michael Hutchinson.</Title>
-      <SubTitle>I like to &lt;code&gt; things for the web.</SubTitle>
-      <Text>A design-minded front-end web developer based in Manchester.</Text>
+      <Name>{config.sections.home.title}</Name>
+      <Title>{config.sections.home.h1}</Title>
+      <SubTitle>{config.sections.home.h2}</SubTitle>
+      <Text>{config.sections.home.p}</Text>
       <Button links="#about" buttonText="Learn more" />
     </section>
   );
